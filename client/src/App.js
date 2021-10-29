3bab62b86ab4f5df32a8b19be4210ee8d3d8a986
Keith Laptop <Keithblack4290@gmail.com>
2021-10-29 16:19:48 -0600

    set up the front end and added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"
+import Instaverse from './images/Instaverse.png'
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>APP</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center" >Instaverse</Typography>
+        <img src={Instaverse} alt="instaverse" heigh="60" />
+
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={4} >
+            <Grid item xs={12} sm={7}>
+            <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+            <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,26 @@
-export const getPosts = (req, res) => {
-    res.send('Post Works!')
+import PostMessage from '../models/postMessage.js'
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+        console.log(postMessages);
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const createPosts = async (req, res) => {
+    const body = req.body;
+
+    const newPost = new PostMessage(body)
+
+    try {
+        await newPost.save()
+
+        res.status(201).json(newPosts)
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+
 }
