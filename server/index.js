82956ca72c39c1c4305f1dc3dc7a7a52369b48d5
Keith Laptop <Keithblack4290@gmail.com>
2021-10-29 15:00:57 -0600

    connected to mongoose! Routes, Controllers, and Models started

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require ('mongoose');
-const cors = require('cors');
+// const express = require('express');
+import express from 'express'
+import bodyParser from'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
+import postRoutes from './routes/posts.js'
 const app = express()
 
 app.use(bodyParser.json({limit:"32mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"32mb", extended:true}));
 app.use(cors());
+
+app.use('/posts', postRoutes)
+
+
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, ({useNewUrlParser:true, useUnifiedTopology:true}))
+    .then(() =>app.listen(PORT, () => console.log(`Server running on prot: ${PORT}`)))
+    .catch(err => console.log(err.message))
