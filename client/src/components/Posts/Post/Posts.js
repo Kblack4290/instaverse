5e0050e3b822239e94df82e108a86e8ce9e1da73
Keith Laptop <Keithblack4290@gmail.com>
2021-10-31 09:46:22 -0600

    added like functionality

--- client/src/components/Posts/Post/Posts.js
+++ client/src/components/Posts/Post/Posts.js
@@ -5,7 +5,7 @@ import { ThumbUpAlt, Delete, MoreHoriz } from '@material-ui/icons'
 import moment from 'moment'
 import { useDispatch } from 'react-redux'
 
-import { deletePost } from '../../../actions/posts.js'
+import { deletePost, likePost } from '../../../actions/posts.js'
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
@@ -14,41 +14,41 @@ const Post = ({ post, setCurrentId }) => {
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia>
-                <div className={classes.overlay}>
-                    <Typography variant="h6">{post.creator}</Typography>
-                    <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-                </div>
-                <div className={classes.overlay2}>
-                    <Button 
-                    style={{ color: "white" }} 
-                    size="small" 
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button
+                    style={{ color: "white" }}
+                    size="small"
                     onClick={() => { setCurrentId(post._id) }}>
-                        <MoreHoriz fontSize="medium" />
-                    </Button>
-                </div>
-                <div className={classes.details}>
-                    <Typography variant="body2" color="textSecondary">
-                        {post.tags.map((tag) => `#${tag} `)}
-                    </Typography>
-                </div>
-                <Typography className={classes.title} variant="h5"
-                    gutterBottom>
-                    {post.title}
+                    <MoreHoriz fontSize="medium" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">
+                    {post.tags.map((tag) => `#${tag} `)}
                 </Typography>
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
-                </CardContent>
-                <CardActions className={classes.cardActions}>
-                    <Button size="small" color="primary" onClick={() => {}}>
-                        <ThumbUpAlt fontSize="small" />
-                        &nbsp;   Like &nbsp; {post.likeCount}
-                    </Button>
-                    <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
-                        <Delete fontSize="small" />
-                        Delete
-                    </Button>
-                </CardActions>
-            
+            </div>
+            <Typography className={classes.title} variant="h5"
+                gutterBottom>
+                {post.title}
+            </Typography>
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
+                    <ThumbUpAlt fontSize="small" />
+                    &nbsp;   Like &nbsp; {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+                    <Delete fontSize="small" />
+                    Delete
+                </Button>
+            </CardActions>
+
         </Card>
     )
 }
