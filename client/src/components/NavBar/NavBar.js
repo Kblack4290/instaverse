d9eb2d4ebbe2141d9e5ab80715c87536b58f6117
Keith Laptop <Keithblack4290@gmail.com>
2021-11-07 14:37:31 -0700

    fixed sign up 500 err. sign in still having issues

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -23,7 +23,6 @@ const NavBar = () => {
     useEffect(() => {
         const token = user?.token
 
-       
 
         setUser(JSON.parse(localStorage.getItem('profile')))
     }, [location])
@@ -37,7 +36,7 @@ const NavBar = () => {
             <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.image.url}>{user.result.name.charAt(0)}</Avatar>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
                         <Button variant="contained" className={classes.logout} onClick={logout} color="secondary">Logout</Button>
                     </div>
