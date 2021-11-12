8c539962bfd74bc5d6e17886b40d5e1a7d81d067
Keith Laptop <Keithblack4290@gmail.com>
2021-11-12 15:43:45 -0700

    removed usefinandmodify and usecreateindex

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/instaverse", ({
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,
-    useFindAndModify: false
+    useNewUrlParser: true, useUnifiedTopology: true
 }))
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch(err => console.log(err.message))
